f92c3f559d7f570f4586baeed32bc45f80a4e204
bshreya15 <i.bakshi.shreya@gmail.com>
2024-10-21 16:32:18 +0100

    code clean & correction

--- practical03/area.c
+++ practical03/area.c
@@ -3,34 +3,29 @@
 
 int main() {
     // Declare variables
-    const M_PI = 3.14732;
     double a = 0.0;                // lower limit (0)
     double b = M_PI / 3.0;         // upper limit (π/3)
     int N = 12;                    // Number of points (including a and b)
     double h = (b - a) / (N - 1);  // Step size (equidistant points)
-    double sum = 0.0;              // Accumulate the result of the trapezoidal sum
-    double x_i;                     // x_i (the i-th point)
-    int i;
 
-    // First part: f(x0) + f(xN)
-    sum = tan(a) + tan(b);  // f(x0) = tan(0), f(xN) = tan(π/3)
+    
+    float sum = tan(a) + tan(b);  // f(x0) = tan(0), f(xN) = tan(π/3)
 
     // Loop over the intermediate points (from x1 to xN-1)
-    for (i = 1; i < N - 1; i++) {
-        x_i = a + i * h;           // Generate the equidistant point x_i
-        sum += 2.0 * tan(x_i);      // Add 2*f(xi) for each intermediate point
+    int i;
+    for ( i = 0; i < 60; i = i + 5)
+    {
+        sum += 2 * tan(i * M_PI / 180);
     }
-
+    
     // Multiply the sum by (b - a) / (2*(N-1))
-    double integral = (b - a) * sum / (2.0 * (N - 1));
-
-    // Actual result from the analytical integral
-    double actual_result = log(2.0);
+    sum = ((b-a)/(2 * N)) * sum;
 
     // Output the computed result and the actual result
-    printf("Computed integral using the trapezoidal rule: %lf\n", integral);
-    printf("Actual result (log(2)): %lf\n", actual_result);
-    printf("Difference: %lf\n", fabs(integral - actual_result));
+    printf("Computed integral using the trapezoidal rule: %lf\n", sum);
+    printf("Actual result (log(2)): %lf\n", logf(2));
+    printf("Absolute Difference: %lf\n", fabs(sum - logf(2)));
+    printf("Relative Difference: %lf\n", fabs(1-(sum/logf(2))));
 
     return 0;
 }
